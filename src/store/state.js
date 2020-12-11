fbcfde5c7a4aca93d2642a55084653faf4084e44
sunshinezjb <zhengjunbo68@126.com>
2020-12-11 22:56:09 +0800

    init repository

---
+++ src/store/state.js
@@ -0,0 +1,12 @@
+export default {
+  callingAPI: false,
+  searching: '',
+  serverURI: 'https://api-prod.com',
+  // user: null,
+  // token: null,
+  // userInfo: {
+  //   messages: [{1: 'test', 2: 'test'}],
+  //   notifications: [],
+  //   tasks: []
+  // }
+}
